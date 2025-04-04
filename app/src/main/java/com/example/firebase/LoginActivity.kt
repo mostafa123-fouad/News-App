6a25d364af7f3fdf93ac1f58610da78a8ef367f0
mostafa123-fouad <mostafashams000444@gmail.com>
2025-04-04 19:28:10 +0200

    LoginActivity.kt

--- app/src/main/java/com/example/firebase/LoginActivity.kt
+++ app/src/main/java/com/example/firebase/LoginActivity.kt
@@ -11,13 +11,14 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        val receivedEmail = intent.getStringExtra("USER_EMAIL")
+        binding.etEmail.setText(receivedEmail)
 
         binding.btnLogin.setOnClickListener {
             val email = binding.etEmail.text.toString().trim()
@@ -38,7 +39,6 @@ class LoginActivity : AppCompatActivity() {
                         } else {
                             Toast.makeText(this, "Login failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                         }
-
                     }
             }
         }
@@ -57,7 +57,7 @@ class LoginActivity : AppCompatActivity() {
 //      super.onStart()
 //   val user = FirebaseAuth.getInstance().currentUser
 //    if (user != null && user.isEmailVerified) {
-//      startActivity(Intent(this, MainActivity::class.java))
+//      startActivity(Intent(this, Category::class.java))
 //         finish()
 //      }
 //  }
